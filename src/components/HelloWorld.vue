ecba57dd0467b6b4bfae0253158ffb20ef013e98
Andrew <andrewosorio589@gmail.com>
2025-03-21 13:37:27 -0600

    CARD

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,94 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="text-center">Gym Tlaxiaco 7w7</h1>
+    
+    <!-- Primer row -->
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-header">
+            <h6>Pesas</h6>
+          </div>
+          <div class="card-body">
+            <p class="card-text">Compuestas por una sola barra con pesos iguales en cada extremo, los mancuernas o haltèress (término inglés italianizado luego también en Italia con la llegada de los deportes como el fitness funcional) son un tipo de equipo para el fitness versátil que ya se utilizaba hace 2000 años!</p>
+          </div>
+        </div>
+      </div>
+      
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-header">
+            <h6>Máquinas</h6>
+          </div>
+          <div class="card-body">
+            <p class="card-text">Desde las populares máquinas de cardio hasta las especializadas en musculación, cada dispositivo tiene un propósito específico que permite trabajar diferentes grupos musculares o mejorar el rendimiento cardiovascular. Esta diversidad no solo facilita personalizar los entrenamientos, sino que también garantiza que tanto principiantes como atletas avanzados encuentren el equipo adecuado para alcanzar sus metas.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <!-- Segundo row -->
+    <div class="row mt-4">
+      <div class="col-md-4">
+        <div class="card">
+          <img src="https://www.cambiatufisico.com/wp-content/uploads/ejercicios-de-gimnasio.jpg" class="card-img-top img-fixed" alt="Ejercicio en gimnasio">
+          <div class="card-body text-center">
+            <p class="card-text">Prensa maquina para hacer pierna y unas de las principales en su uso.</p>
+            <button class="btn btn-warning">Mas informacion aqui</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <img src="https://getfit.mx/cdn/shop/files/IMG-0141.png?v=1735613264&width=533" class="card-img-top img-fixed" alt="Ejercicio en gimnasio">
+          <div class="card-body text-center">
+            <p class="card-text">Ropa de gym lo mejor d elo mejor youngla.</p>
+            <button class="btn btn-warning">Mas informacion aqui</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <img src="https://hips.hearstapps.com/hmg-prod/images/professional-bodybuilder-arnold-schwarzenegger-posing-at-news-photo-481406443-1547849341.jpg?resize=980:*" class="card-img-top img-fixed" alt="Ejercicio en gimnasio">
+          <div class="card-body text-center">
+            <p class="card-text">Hiostoria del fisiculturismo.</p>
+            <button class="btn btn-warning">Mas informacion aqui</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <!-- Tercer row centrado -->
+    <div class="row mt-4 justify-content-center">
+      <div class="col-md-4">
+        <div class="card">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9lOD8fZLTzoSSHja7uB8uZuConInXOA9Tiw&s" class="card-img-top img-fixed" alt="Ejercicio en gimnasio">
+          <div class="card-body text-center">
+            <p class="card-text">Dietas para una vida saludable y un mejor endimeinto.</p>
+            <button class="btn btn-warning">Mas informacion aqui</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuRYvHXXOO2wkiFKi-EHATSUZV-N0CAh_iNQ&s" class="card-img-top img-fixed" alt="Ejercicio en gimnasio">
+          <div class="card-body text-center">
+            <p class="card-text">Mensualidad de gymnasisos,precios y mas.</p>
+            <button class="btn btn-warning">Mas informacion aqui</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.img-fixed {
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+}
+</style>
