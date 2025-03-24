a2d9e008320924a3cc9e48f667f323a3510700ca
andrew <andrewosorio589@gmail.com>
2025-03-24 15:56:48 -0600

    Agregamos las cards

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,7 +2,9 @@
      <div class="row mt-4">
       <div class="col-md-4">
         <div class="card">
-          <img src="https://www.cambiatufisico.com/wp-content/uploads/ejercicios-de-gimnasio.jpg" class="card-img-top img-fixed" alt="Ejercicio en gimnasio">
+          <img src="https://www.cambiatufisico.com/wp-content/uploads/ejercicios-de-gimnasio.jpg" class="card-img-top img-fluid rounded mx-auto d-block" 
+     style="max-width: 300px; max-height: 200px;" 
+     alt="Ejercicio en gimnasio">
           <div class="card-body text-center">
             <p class="card-text">Prensa maquina para hacer pierna y unas de las principales en su uso.</p>
             <button class="btn btn-warning">Mas informacion aqui</button>
@@ -20,7 +22,9 @@
       </div>
       <div class="col-md-4">
         <div class="card">
-          <img src="https://hips.hearstapps.com/hmg-prod/images/professional-bodybuilder-arnold-schwarzenegger-posing-at-news-photo-481406443-1547849341.jpg?resize=980:*" class="card-img-top img-fixed" alt="Ejercicio en gimnasio">
+          <img src="https://hips.hearstapps.com/hmg-prod/images/professional-bodybuilder-arnold-schwarzenegger-posing-at-news-photo-481406443-1547849341.jpg?resize=980:*"  class="card-img-top img-fluid rounded mx-auto d-block" 
+     style="max-width: 300px; max-height: 200px;" 
+     alt="Ejercicio en gimnasio">
           <div class="card-body text-center">
             <p class="card-text">Hiostoria del fisiculturismo.</p>
             <button class="btn btn-warning">Mas informacion aqui</button>
